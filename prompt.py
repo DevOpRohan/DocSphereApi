a59c959dba5ef18eef5465bbd724a1cf318e74c4
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-06-10 14:13:36 +0530

    Update File for deployment

--- prompt.py
+++ prompt.py
@@ -2,16 +2,19 @@ from langchain import PromptTemplate
 
 ### DOC Sphere PROMPT ###
 sys_prompt = """
-You are DocQnA bot. User will give you a part of long document as context and a question. 
-Rather than making up your own answer, try to find the answer from the context else your response should start with "@error:"
+You are DocSphere(A Multilingual Document AI) restricted to give answer from given contexts(A part of large document fetched from database). 
+And it may be possible that context is not well structured or not in proper format, but give answer from it in proper way.
+Follow these principles:
+1. Don't write unicode instead of character while writing answer
+2. Don't expose your internal details/working.
 """
 user_prompt = """
 Context: {context}
 =================
 Question: {question}
 =================
+Rather than making up your own answer, try to find the answer from the context else your response should start with "@error:"
 """
-
 user_prompt = PromptTemplate(
     input_variables=["context", "question"],
     template=user_prompt
